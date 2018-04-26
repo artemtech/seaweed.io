a2b9955e2cb01c8380a61bd1c7fd5f20e7c0b61c
Sofyan Sugianto <sofyanartem@gmail.com>
2018-04-26 22:14:52 +0700

    add helper scripts for player and nodes

---
+++ Scripts/Helper/Utils.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var _current_screen = ""
+var _player = {
+	"name":"nganu",
+	"company":"sae",
+	"uang":100
+}
+
+func find_node_with_group(group, recursive=true):
+	for child in get_children():
+		if child.is_in_group(group):
+			return child
+		if recursive:
+			var node = child.find_node_with_group(group,true)
+			if node != null:
+				return node
+	pass
+
+func set_player(var player={}):
+	_player = player
