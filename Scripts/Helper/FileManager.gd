c130828ce1c3b58cce709276c451366b62762b29
Sofyan Sugianto <sofyanartem@gmail.com>
2018-04-27 01:01:14 +0700

    add FileManager base for db

---
+++ Scripts/Helper/FileManager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+func save_data(player):
+	pass
+
+func get_data():
+	return
+	pass
+
+func delete_data(player):
+	pass
+
+func load_data(player):
+	return 
+	pass
+	
+func _data_writer():
+	pass
+
+func _data_reader():
+	pass
